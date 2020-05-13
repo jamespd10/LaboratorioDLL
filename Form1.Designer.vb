8b4c002f4c0e0ac9b6b96181970ff4a39acd026c
unknown <jamespd10@gmail.com>
2020-05-13 10:09:58 -0500

    .

---
+++ Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDato1 = New System.Windows.Forms.TextBox()
+        Me.txtDato2 = New System.Windows.Forms.TextBox()
+        Me.txtRespuesta = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnRestar = New System.Windows.Forms.Button()
+        Me.btnMultiplicar = New System.Windows.Forms.Button()
+        Me.btnDividir = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtDato1
+        '
+        Me.txtDato1.Location = New System.Drawing.Point(146, 64)
+        Me.txtDato1.Name = "txtDato1"
+        Me.txtDato1.Size = New System.Drawing.Size(100, 20)
+        Me.txtDato1.TabIndex = 0
+        '
+        'txtDato2
+        '
+        Me.txtDato2.Location = New System.Drawing.Point(146, 108)
+        Me.txtDato2.Name = "txtDato2"
+        Me.txtDato2.Size = New System.Drawing.Size(100, 20)
+        Me.txtDato2.TabIndex = 1
+        '
+        'txtRespuesta
+        '
+        Me.txtRespuesta.Location = New System.Drawing.Point(146, 148)
+        Me.txtRespuesta.Name = "txtRespuesta"
+        Me.txtRespuesta.ReadOnly = True
+        Me.txtRespuesta.Size = New System.Drawing.Size(100, 20)
+        Me.txtRespuesta.TabIndex = 2
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(36, 211)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 3
+        Me.btnCalcular.Text = "Sumar"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(36, 293)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 4
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(104, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 25)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Cálculo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(44, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Dato 1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(44, 151)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(58, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Respuesta"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(44, 111)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Dato 2"
+        '
+        'btnRestar
+        '
+        Me.btnRestar.Location = New System.Drawing.Point(171, 211)
+        Me.btnRestar.Name = "btnRestar"
+        Me.btnRestar.Size = New System.Drawing.Size(75, 23)
+        Me.btnRestar.TabIndex = 9
+        Me.btnRestar.Text = "Restar"
+        Me.btnRestar.UseVisualStyleBackColor = True
+        '
+        'btnMultiplicar
+        '
+        Me.btnMultiplicar.Location = New System.Drawing.Point(36, 253)
+        Me.btnMultiplicar.Name = "btnMultiplicar"
+        Me.btnMultiplicar.Size = New System.Drawing.Size(75, 23)
+        Me.btnMultiplicar.TabIndex = 10
+        Me.btnMultiplicar.Text = "Multiplicar"
+        Me.btnMultiplicar.UseVisualStyleBackColor = True
+        '
+        'btnDividir
+        '
+        Me.btnDividir.Location = New System.Drawing.Point(171, 253)
+        Me.btnDividir.Name = "btnDividir"
+        Me.btnDividir.Size = New System.Drawing.Size(75, 23)
+        Me.btnDividir.TabIndex = 11
+        Me.btnDividir.Text = "Dividir"
+        Me.btnDividir.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(171, 293)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 12
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 350)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnDividir)
+        Me.Controls.Add(Me.btnMultiplicar)
+        Me.Controls.Add(Me.btnRestar)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtRespuesta)
+        Me.Controls.Add(Me.txtDato2)
+        Me.Controls.Add(Me.txtDato1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtDato1 As TextBox
+    Friend WithEvents txtDato2 As TextBox
+    Friend WithEvents txtRespuesta As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnRestar As Button
+    Friend WithEvents btnMultiplicar As Button
+    Friend WithEvents btnDividir As Button
+    Friend WithEvents btnSalir As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports ClassLibrary1
+Public Class Form1
+    Dim obj_s As New Class1
+    Public Sub limpiar()
+        txtDato1.Text = ""
+        txtDato2.Text = ""
+        txtRespuesta.Text = ""
+    End Sub
+    Private Sub BtnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        limpiar()
+    End Sub
+
+    Private Sub BtnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim resultado As Integer
+        obj_s.dato1() = CInt(txtDato1.Text)
+        obj_s.dato2() = CInt(txtDato2.Text)
+        resultado = obj_s.Sumar_Numeros()
+        txtRespuesta.Text = resultado
+    End Sub
+
+    Private Sub BtnRestar_Click(sender As Object, e As EventArgs) Handles btnRestar.Click
+        Dim resultado As Integer
+        obj_s.dato1() = CInt(txtDato1.Text)
+        obj_s.dato2() = CInt(txtDato2.Text)
+        resultado = obj_s.Restar_Numeros()
+        txtRespuesta.Text = resultado
+    End Sub
+
+    Private Sub BtnMultiplicar_Click(sender As Object, e As EventArgs) Handles btnMultiplicar.Click
+        Dim resultado As Integer
+        obj_s.dato1() = CInt(txtDato1.Text)
+        obj_s.dato2() = CInt(txtDato2.Text)
+        resultado = obj_s.Multiplicar_Numeros()
+        txtRespuesta.Text = resultado
+    End Sub
+
+    Private Sub BtnDividir_Click(sender As Object, e As EventArgs) Handles btnDividir.Click
+        Dim resultado As Integer
+        obj_s.dato1() = CInt(txtDato1.Text)
+        obj_s.dato2() = CInt(txtDato2.Text)
+        If (CInt(txtDato2.Text) <= 0) Then
+            MsgBox("Debe ser un número mayor que 0.")
+            limpiar()
+        Else
+            resultado = obj_s.Dividir_Numeros()
+            txtRespuesta.Text = resultado
+        End If
+    End Sub
+
+    Private Sub TxtDato1_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtDato1.Validating
+        If (Not IsNumeric(txtDato1.Text)) Then
+            MsgBox("Debe insertar solo número.")
+            e.Cancel = True
+            limpiar()
+        End If
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+
+    Private Sub TxtDato2_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtDato2.Validating
+        If (Not IsNumeric(txtDato2.Text)) Then
+            MsgBox("Debe insertar solo número.")
+            txtDato2.Text = ""
+            e.Cancel = True
+        End If
+    End Sub
+End Class
